4841d278fef63c67dbb9d2832026fb9a01b75991
Vitor Nascimento <vitor.silva.nascimento@santander.com.br>
2021-11-05 20:37:51 -0300

    implement initial main screen layout

---
+++ app/src/main/java/me/vitornascimento/behealthy/MainFragment.kt
@@ -0,0 +1,50 @@
+package me.vitornascimento.behealthy
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import me.vitornascimento.behealthy.databinding.FragmentMainBinding
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+  private var _binding: FragmentMainBinding? = null
+  private val binding get() = _binding!!
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    _binding = FragmentMainBinding.bind(view)
+    initializeView()
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+
+  private fun initializeView() {
+    val adapter = ArrayAdapter(
+      requireContext(),
+      R.layout.hospital_categories_list_item,
+      resources.getStringArray(R.array.hospital_categories)
+    )
+
+    with(binding.dropdownMenu.editText as AutoCompleteTextView) {
+      setAdapter(adapter)
+      setOnItemClickListener { _, _, _, _ ->
+        binding.btnFindHospital.isEnabled = true
+      }
+    }
+
+    binding.btnFindHospital.setOnClickListener {
+      Toast.makeText(
+        requireContext(),
+        (binding.dropdownMenu.editText as AutoCompleteTextView).text.toString(),
+        Toast.LENGTH_SHORT
+      ).show()
+    }
+  }
+
+}
